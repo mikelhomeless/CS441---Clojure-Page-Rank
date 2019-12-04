dd41e4ecdb040a00e0d4fb6b8d372f1bcd55403b
mikelhomeless <mthomer1027@gmail.com>
2019-12-04 13:32:14 -0600

    added new file structure and initial page ranks

--- src/pagerank/core.clj
+++ src/pagerank/core.clj
@@ -1,19 +1,24 @@
-(ns rescue.core
+(ns pagerank.core
   (:require [clojure.java.io :as io])
   (:require [clojure.string :as str]))
 
-(def outdata
+(def outlinks
   (with-open [rdr (io/reader "test.txt")]
   (into {} (map #(vector (first %) (rest %))
            (map #(str/split % #" ") (line-seq rdr))))))
 
-(def indata
+(def inlinks
   (apply merge-with into
     (flatten
       (reduce-kv
         (fn [ncoll k v]
             (conj ncoll (into [] (map #(hash-map % (vector k)) v))))
         []
-        outdata))))
+        outlinks))))
 
-(def pageRanks)
+;; since each page has a weight of 1 at the beginning,
+;; we go ahead and just sum up the inlink counts as the page rank for each page
+(def pageRanks
+  (into {} (map
+              (fn [[page links]] (vector page (count links)))
+              inlinks)))
